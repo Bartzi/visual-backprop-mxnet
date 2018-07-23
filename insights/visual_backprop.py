1aae42373e4776c22f0b0e8bcf1d81f4360f42ef
Michael Yahalom <michael_yahalom@yahoo.com>
2018-07-23 21:14:25 +0300

    fixes for strided conv and intermediate norm

--- insights/visual_backprop.py
+++ insights/visual_backprop.py
@@ -11,10 +11,27 @@ def combine_feature_maps(intermediate_symbols, node, nodes, scaled_feature):
     data_input_node = nodes[node['inputs'][0][0]]
     try:
         data_input_symbol = intermediate_symbols["{}_output".format(data_input_node['name'])]
+#    except ValueError:
+#        data_input_symbol = intermediate_symbols[data_input_node['name']]
     except ValueError:
-        data_input_symbol = intermediate_symbols[data_input_node['name']]
+        try:
+            data_input_symbol = intermediate_symbols[data_input_node['name']]
+        except ValueError:
+            data_input_symbol = intermediate_symbols["{}_output0".format(data_input_node['name'])] # MY looks like lrn has output0
+
+
     averaged_feature_map = mx.symbol.mean(data_input_symbol, axis=1, keepdims=True)
-    feature_map = scaled_feature * averaged_feature_map
+	
+    # MY normalization test- by bojarski comment from 16/11/17 on https://github.com/mbojarski/VisualBackProp/issues/1
+    min_value = mx.symbol.min(averaged_feature_map)
+    max_value = mx.symbol.max(averaged_feature_map)
+    averaged_feature_map = mx.symbol.broadcast_sub(averaged_feature_map, min_value)
+    averaged_feature_map = mx.symbol.broadcast_mul(averaged_feature_map, 1.0 / (max_value - min_value))
+    # end MY normalization test
+
+    fixed_scaled_feature = mx.symbol.Crop(scaled_feature, averaged_feature_map)
+
+    feature_map = fixed_scaled_feature * averaged_feature_map
     return feature_map
 
 
@@ -27,37 +44,53 @@ def build_visual_backprop_symbol(start_symbol, input_name=None):
     intermediate_symbols = start_symbol.get_internals()
 
     feature_map = mx.symbol.mean(start_symbol, axis=1, keepdims=True)
+
+    # MY normalization test- by bojarski comment from 16/11/17 on https://github.com/mbojarski/VisualBackProp/issues/1
+    min_value = mx.symbol.min(feature_map)
+    max_value = mx.symbol.max(feature_map)
+    feature_map = mx.symbol.broadcast_sub(feature_map, min_value)
+    feature_map = mx.symbol.broadcast_mul(feature_map, 1.0 / (max_value - min_value))
+    # end MY normalization test
+
     for node in reversed(nodes):
         if input_name is not None and node['name'] == input_name or node['name'] == 'data':
-            break
+            breakPagey
 
-        node_attrs = node.get('attr', None)
+        node_attrs = node.get('attrs', None) # MY was 'attr' until around mxnet 1.0
         if node['op'] == 'Convolution':
             kernel_height, kernel_width = map(int, string_to_tuple(node_attrs['kernel']))
+            stride_height, stride_width = map(int, string_to_tuple(
+                node_attrs.get('stride', '(1, 1)')))  # MY added for stride in convolution
             pad_height, pad_width = map(int, string_to_tuple(node_attrs.get('pad', '(0, 0)')))
             scaled_feature = mx.symbol.Deconvolution(
                 data=feature_map,
                 weight=mx.symbol.ones((1, 1, kernel_height, kernel_width)),
                 kernel=(kernel_height, kernel_width),
                 pad=(pad_height, pad_width),
+                stride=(stride_height, stride_width),  # MY added this- was missing???
+                adj=(stride_height - 1, stride_width - 1),
                 num_filter=1
             )
             feature_map = combine_feature_maps(intermediate_symbols, node, nodes, scaled_feature)
         elif node['op'] == 'Pooling':
             kernel_height, kernel_width = map(int, string_to_tuple(node_attrs['kernel']))
-            stride_height, stride_width = map(int, string_to_tuple(node_attrs['stride']))
+            #MY stride_height, stride_width = map(int, string_to_tuple(node_attrs['stride']))
+            stride_height, stride_width = map(int, string_to_tuple(
+                node_attrs.get('stride', '(1, 1)')))  # MY need default- theoretically could be pooling without stride..
             scaled_feature = mx.symbol.Deconvolution(
                 data=feature_map,
                 weight=mx.symbol.ones((1, 1, kernel_height, kernel_width)),
                 kernel=(kernel_height, kernel_width),
                 stride=(stride_height, stride_width),
+                adj=(stride_height - 1,
+                     stride_width - 1),  # MY this is sometimes needed- will take care of it with Crop if needed in combine_feature_maps
                 num_filter=1,
             )
             feature_map = combine_feature_maps(intermediate_symbols, node, nodes, scaled_feature)
         else:
             continue
 
-    # normalize feature map
+    # normalize feature map - (MY this isn't usually really necessary if we normalize in combine_feature_maps, but shouldn't hurt)
     min_value = mx.symbol.min(feature_map)
     max_value = mx.symbol.max(feature_map)
     feature_map = mx.symbol.broadcast_sub(feature_map, min_value)
